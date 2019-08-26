661f02296900da250437c787d5be9e3ee5a96bd9
Stavros Pitoglou <s.pitoglou@csl.gr>
2019-08-27 00:42:34 +0300

    Prepare for team stats

--- soccer1.py
+++ soccer1.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+import pandas as pd
 # import numpy as np
 from lib.championships import load_greece
 from lib import period_stats, create_team_df_dict
@@ -12,3 +12,15 @@ print(team_dfs['Olympiakos'])
 
 
 print(period_stats(team_dfs['Panionios'], '1819'))
+
+a = team_dfs['Atromitos']
+a.replace('', 0, inplace=True)
+print(a['count_no_draw'].max(), a.iloc[-1, :]['count_no_draw'])
+
+new = pd.DataFrame()
+new = new.append({
+    'Name': 'Atromitos',
+    'MaxNoDraw': a['count_no_draw'].max(),
+    'CurrentNoDraw': a.iloc[-1, :]['count_no_draw']
+}, ignore_index=True)
+print(new)
