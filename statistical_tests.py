35e3e2963ea021fa0ec81dba4e0056c7ac18eb90
Stavros Pitoglou <s.pitoglou@csl.gr>
2019-09-05 17:35:58 +0300

    half way through bayesian tests

--- statistical_tests.py
+++ statistical_tests.py
@@ -1,13 +1,13 @@
-from external import thinkplot
-from collections import Counter
-from sp_soccer_lib.championships import load_england, load_italy, team_stats, load_country
-from sp_soccer_lib.handout_helpers import style, get_country_header, make_link
-from sp_soccer_lib import create_team_df_dict, championship_teams, no_draw_frequencies, create_team_df, update_draw_streaks, update_results
-from sp_soccer_lib.probabilities import cumulative_binomial_probabilities
-from external import thinkstats2
+# from external import thinkplot
+# from collections import Counter
+# from sp_soccer_lib.championships import load_england, load_italy, team_stats, load_country
+# from sp_soccer_lib.handout_helpers import style, get_country_header, make_link
+# from sp_soccer_lib import create_team_df_dict, championship_teams, no_draw_frequencies, create_team_df, update_draw_streaks, update_results
+# from sp_soccer_lib.probabilities import cumulative_binomial_probabilities
+# from external import thinkstats2
 
-import pprint as pp
-import pandas as pd
+# import pprint as pp
+# import pandas as pd
 
 '''
 next_matches = 7
@@ -48,5 +48,7 @@ for val in sorted(hist.Values()):
 thinkplot.Hist(hist)
 thinkplot.Show(xlabel='value', ylabel='frequency')
 '''
+
+from external.thinkbayes import Pmf
 
 
